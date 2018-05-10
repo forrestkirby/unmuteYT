009e257170ba2066d8cf99e6bbbca2ff6209ee10
forrestkirby <18228336+forrestkirby@users.noreply.github.com>
2018-05-10 13:09:28 +0200

    Add files via upload

---
+++ unmuteYT.js
@@ -0,0 +1,93 @@
+// author: forrestkirby https://github.com/forrestkirby
+
+var ytPlayer = null,
+    ytButton = null,
+    ytRange  = null,
+    ytImage  = null;
+
+document.addEventListener('DOMContentLoaded', function() {
+
+	ytButton = document.getElementById('yt-button'),
+	ytRange  = document.getElementById('yt-range'),
+	ytImage  = document.getElementById('yt-image');
+
+	document.querySelector('#bgvideo iframe').id = 'yt-player';
+
+});
+
+function onYouTubeIframeAPIReady() {
+
+	ytPlayer = new YT.Player('yt-player');
+
+}
+
+function switchYtButton(hasSound) {
+
+	if (hasSound) {
+
+		if ( ytImage ) {
+
+			ytImage.src = 'images/speaker.svg';
+			ytImage.alt = 'mute';
+
+		} else {
+
+			ytButton.innerHTML = 'mute';
+
+		}
+
+	} else if ( ytImage ) {
+
+			ytImage.src = 'images/speaker_muted.svg';
+			ytImage.alt = 'unmute';
+
+	} else {
+
+			ytButton.innerHTML = 'unmute';
+
+	}
+
+}
+
+function toggleYtPlayerSound() {
+
+	if (ytRange.value == 0) {
+
+			ytPlayer.setVolume(50);
+			ytRange.value = 50;
+
+	}
+
+	if (ytPlayer.isMuted()) {
+
+		ytPlayer.unMute();
+		switchYtButton(true);
+
+	} else {
+
+		ytPlayer.mute();
+		switchYtButton(false);
+
+	}
+
+}
+
+function setYtPlayerVolume(vol) {
+
+	ytPlayer.setVolume(vol);
+
+	if (ytPlayer.isMuted()) {
+
+		ytPlayer.unMute();
+		switchYtButton(true);
+
+	}
+
+	if (vol == 0) {
+
+		ytPlayer.mute();
+		switchYtButton(false);
+
+	}
+
+}
